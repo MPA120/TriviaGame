19ed260674f838bf9360865d2d0a8bf3f6a6b9fa
MPA <50469825+MPA120@users.noreply.github.com>
2019-07-30 12:44:24 -0400

    updated html and starting JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var Questions = [{
+    question: "What year was NASA founded?",
+    answerList: ["1943", "1972","1958","1954"],
+    answer: 2
+
+},{
+
+    question: "How many Apollo missions were there?",
+    answerList: ['5', '6', '2', '13'],
+    answer: 1
+}]
