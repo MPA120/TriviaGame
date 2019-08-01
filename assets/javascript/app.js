6f886c868d767b688eb9caea7d5dda98b75194f8
MPA <50469825+MPA120@users.noreply.github.com>
2019-07-31 20:08:02 -0400

    html update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,16 @@
-var Questions = [{
+$( document ).ready(function() {
+
+var number = 60;
+
+var intervalID;
+
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+
+
+// Trivia Questions
+var questions = [{
     question: "What year was NASA founded?",
     answerList: ["1943", "1972","1958","1954"],
     answer: 2
@@ -27,22 +39,203 @@ var Questions = [{
 
 }];
 
-var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userAnswer;
-var messages = {
-    correct: "Affirmative",
-    incorrect: "Negative",
-    endTime: "Mission Over",
-    finished: "Mission Complete"
+$('#startBtn').on("click", function() {
+    $(this).hide();
+    $('#time').html("<h2>Time Remaining: 60 seconds</h2>" + "<br>");
+
+    run();
+
+    $("#question1").html("<h3>" + questions[0].question + "</h3>");
+    $("#answer1").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[0].answerList[0] + "</label>"
+        + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[0].answerList[1] + "</label>"
+        + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[0].answerList[2] + "</label>"
+        + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[0].answerList[3] + "</label><br><br>"
+    );
+
+    $("#question2").html("<h3>" + questions[1].question + "</h3>");
+    $("#answer2").html("<input type='radio' name='answer2' value='0'>" + "<label>" + questions[1].answerList[0] + "</label>"
+        + "<input type='radio' name='answer2' value='1'>" + "<label>" + questions[1].answerList[1] + "</label>"
+        + "<input type='radio' name='answer2' value='2'>" + "<label>" + questions[1].answerList[2] + "</label>"
+        + "<input type='radio' name='answer2' value='3'>" + "<label>" + questions[1].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 3
+    $("#question3").html("<h3>" + questions[2].question + "</h3>");
+    $("#answer3").html("<input type='radio' name='answer3' value='0'>" + "<label>" + questions[2].answerList[0] + "</label>"
+        + "<input type='radio' name='answer3' value='1'>" + "<label>" + questions[2].answerList[1] + "</label>"
+        + "<input type='radio' name='answer3' value='2'>" + "<label>" + questions[2].answerList[2] + "</label>"
+        + "<input type='radio' name='answer3' value='3'>" + "<label>" + questions[2].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 4
+    $("#question4").html("<h3>" + questions[3].question + "</h3>");
+    $("#answer4").html("<input type='radio' name='answer4' value='0'>" + "<label>" + questions[3].answerList[0] + "</label>"
+        + "<input [type='radio'] name='answer4' value='1'>" + "<label>" + questions[3].answerList[1] + "</label>"
+        + "<input type='radio' name='answer4' value='2'>" + "<label>" + questions[3].answerList[2] + "</label>"
+        + "<input type='radio' name='answer4' value='3'>" + "<label>" + questions[3].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 5
+    $("#question5").html("<h3>" + questions[4].question + "</h3>");
+    $("#answer5").html("<input type='radio' name='answer5' value='0'>" + "<label>" + questions[4].answerList[0] + "</label>"
+        + "<input type='radio' name='answer5' value='1'>" + "<label>" + questions[4].answerList[1] + "</label>"
+        + "<input type='radio' name='answer5' value='2'>" + "<label>" + questions[4].answerList[2] + "</label>"
+        + "<input type='radio' name='answer5' value='3'>" + "<label>" + questions[4].answerList[3] + "</label><br><br>"
+    );
+    // Question 6
+    $("#question6").html("<h3>" + questions[5].question + "</h3>");
+    $("#answer6").html("<input type='radio' name='answer6' value='0'>" + "<label>" + questions[5].answerList[0] + "</label>"
+        + "<input type='radio' name='answer6' value='1'>" + "<label>" + questions[5].answerList[1] + "</label>"
+        + "<input type='radio' name='answer6' value='2'>" + "<label>" + questions[5].answerList[2] + "</label>"
+        + "<input type='radio' name='answer6' value='3'>" + "<label>" + questions[5].answerList[3] + "</label><br><br>"
+    
+    );
+
+    $("#submit").html("<button id='done' class='btn'>Done</button>");
+    $("#done").on('click', function(){
+        keepingScore();
+        displayResults();
+
+
+    });
+});
+
+function run() {
+
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
 }
 
-$('#startBtn').on('click', function(){
-    $(this).hide()
-    newGame();
-});
-    
+function decrement(){
+    $('#time').html('<h2>Time Remaining:' + number + "Seconds</h2>" + "<br>");
+
+    if (number === 0) {
+        stop();
+
+        keepingScore();
+        displayResults();
+    }
+}
+
+function stop() {
+    clearInterval(intervalID);
+}
+
+function displayResults() {
+
+    $("#time").hide();
+    $("#question1").hide();
+    $("#answer1").hide();
+    $("#question2").hide();
+    $("#answer2").hide();
+    $("#question3").hide();
+    $("#answer3").hide();
+    $("#question4").hide();
+    $("#answer4").hide();
+    $("#question5").hide();
+    $("#answer5").hide();
+    $("#question6").hide();
+    $("#answer6").hide();
+    $("#submit").hide();
+
+    $("#resultsMessage").html("<h3>All Done!</h3>");
+    $("#correct").html("Correct Answers: " + correctAnswers);
+    $("#incorrect").html("Incorrect Answers: " + incorrectAnswers);
+    $("#unanswered").html("Unanswered: " + unanswered);
+}
+
+function keepingScore() {
+
+    var userAnswer1 = $("input[name='answer1']:checked").val();
+    var userAnswer2 = $("input[name='answer2']:checked").val();
+    var userAnswer3 = $("input[name='answer3']:checked").val();
+    var userAnswer4 = $("input[name='answer4']:checked").val();
+    var userAnswer5 = $("input[name='answer5']:checked").val();
+    var userAnswer6 = $("input[name='answer6']:checked").val();
 
 
+    // Question 1
+    if (userAnswer1 === undefined) {
 
+        unanswered++;
+    }
+    else if (userAnswer1 == questions[0].answer) {
 
+        correctAnswers++;
+    }
+    else {
 
+        incorrectAnswers++;
+    }
 
+    // Question 2
+    if (userAnswer2 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer2 == questions[1].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 3
+    if (userAnswer3 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer3 == questions[2].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 4
+    if (userAnswer4 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer4 == questions[3].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 5
+    if (userAnswer5 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer5 == questions[4].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 6
+    if (userAnswer6 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer6 == questions[5].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+}
+})
